ca5588d5b77db2a15a2cd84dae86dea56f452434
ma-alvarez <magual89@gmail.com>
2015-06-16 10:21:05 -0300

    adding configuration to run migrations y production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '10.120.79.19', user: 'malvarez', roles: %w{web app}
+server '10.120.79.19', user: 'malvarez', roles: %w{web app db}
