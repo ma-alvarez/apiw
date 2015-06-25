4fe27c9708ba4133609b8ad7a6e4c02309f61700
ma-alvarez <magual89@gmail.com>
2015-06-25 15:06:32 -0300

    adding id to dcvs

--- app/models/dcv.rb
+++ app/models/dcv.rb
@@ -2,7 +2,7 @@ class Dcv < ActiveRecord::Base
 	has_one :status
 
 	def as_json(options = {})
-		super(only:[:cpu,:memory,:hard_disk,:bw_avg_in,:bw_avg_out,:bw_peak_in,
+		super(only:[:id,:cpu,:memory,:hard_disk,:bw_avg_in,:bw_avg_out,:bw_peak_in,
         :bw_peak_out,:public_ip_count,:ip_net_web,:ip_net_application,:ip_net_backend,
         :edge_high_availability])	
 	end
