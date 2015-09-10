5244d3124a47e0d0391f2b603eb888f5b6fc19ec
ma-alvarez <magual89@gmail.com>
2015-09-10 15:33:17 -0300

    setting root user in deployment file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '10.120.164.2', user: 'malvarez', roles: %w{web app db}
-server '10.120.164.3', user: 'malvarez', roles: %w{web app }
+server '10.120.164.2', user: 'root', roles: %w{web app db}
+server '10.120.164.3', user: 'root', roles: %w{web app }
