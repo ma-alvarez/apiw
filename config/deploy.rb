310ede25c392b1fffb5fec39f3054adc8cd5a8d6
ma-alvarez <magual89@gmail.com>
2015-09-10 15:41:16 -0300

    setting production branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.2.1'
 
 set :application, 'apiw'
 set :repo_url, 'git@github.com:ma-alvarez/apiw.git'
-
+set :branch, 'production'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
